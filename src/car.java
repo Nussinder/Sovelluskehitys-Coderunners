fe2ed6bcd8f6a485a090d9cae3391f2fcacd235b
Nussinder <jonathan.nikander@gmail.com>
2021-12-16 20:50:37 +0200

    Initial commit

---
+++ src/car.java
@@ -0,0 +1,45 @@
+class Car {
+
+	public String brand = "Audi";
+	public String model = "A5";
+	public int amountOfFuel = 0;
+	public int fuelDecrease = -1;
+	 
+	
+	public void brake() {
+
+		System.out.println("Car uses brakes.");
+
+	}
+
+	public void accelerate() {
+
+		System.out.println("Car is accelerating whit " + amountOfFuel + " gallons of fuel.");
+		
+			if ( amountOfFuel == 0  ) {
+				
+				System.out.println( "Insufficient fuel, gas refill required. ");
+			}
+
+	}
+
+	public void fueldecreasing() {
+
+		fuelDecrease = -1;
+		System.out.println("fuel has decreased from " + amountOfFuel + " gallons to " + (amountOfFuel + fuelDecrease) + " gallons." );
+		
+		if (amountOfFuel == 0 ) {
+			System.out.println("please refill the damn vechicle.");
+		}
+
+	}
+
+	public void printData() {
+
+		System.out.println("Brand: " + brand + ", Model: " + model + ", Amount of fuel " + amountOfFuel + " gallons");
+		System.out.println("");
+	}
+
+}
+
+
---
+++ src/carmain.java
@@ -0,0 +1,20 @@
+
+public class carmain {
+
+	public static void main(String[] args) {
+		
+		//Calls the methods below
+
+		Car car1 = new Car();
+		
+		car1.printData();
+		car1.accelerate();
+		car1.brake();
+		car1.fueldecreasing();
+		
+	}
+
+} // end on main 
+
+
+// Classes are in the car.java
